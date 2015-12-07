a0ce6660a5d49bebdc38621ca1eeb02fc56b916e
pearpages <pere@soms.cat>
2015-12-07 22:04:26 +0100

    teamdetail controller

--- www/app/teams/team-detail.controller.js
+++ www/app/teams/team-detail.controller.js
@@ -7,14 +7,21 @@
 	function TeamDetailController ($stateParams,eliteApi,$ionicPopup){
 		var vm = this;
 
-		vm.teamId = Number($stateParams.id);
-		vm.teamName = getTeamName();
-		vm.games = getGames();
-		vm.teamStanding = getTeamStanding();
+		vm.teamId = null;
+		vm.teamName = null;
+		vm.games = null;
+		vm.teamStanding = null;
 		vm.toggleFollow = toggleFollow;
 		vm.following = null;
 
-		var data;
+		activate();
+
+		function activate() {
+			vm.teamId = Number($stateParams.id);
+			vm.teamStanding = getTeamStanding();
+			vm.teamName = getTeam().name;
+			vm.games = getGames();
+		}
 
 		function toggleFollow(){
 			if(vm.following){
@@ -33,39 +40,51 @@
 		}
 
 
-		function getData(){
-			if(!data){
-				data = eliteApi.getLeagueData();
-			}
-
-			return data;
+		function getLeagueData(){
+			return eliteApi.getLeagueData();
 		} 
 
 		function getTeamStanding(){
-			return _.chain(getData().standings)
-					.flatten("divisionStandings")
-					.find({"teamId": vm.teamId})
-					.value();
+			var divisions = getLeagueData().standings;
+			var standings;
+			var standing;
+			for (var i = 0; i<divisions.length; i++) {
+				standings = divisions[i].divisionStandings;
+				for (var j = 0; j<standings.length; j++) {
+					standing = standings[j];
+					if(standing.teamId === vm.teamId){
+						return standing;
+					}
+				}
+			}
 		}
 
-		function getTeamName(){
-			return _.chain(getData().teams)
-					.flatten("divisionTeams")
-					.find({"id": vm.teamId})
-					.value();
+		function getTeam(){
+			var divisions = getLeagueData().teams;
+			var divisionTeams;
+			var team;
+			for (var i = 0; i<divisions.length; i++) {
+				divisionTeams = divisions[i].divisionTeams;
+				for (var j = 0; j<divisionTeams.length; j++) {
+					team = divisionTeams[j];
+					if(team.id === vm.teamId){
+						return team;
+					}
+				}
+			}
 		}
 		
 		function getGames(){
-			return _.chain(getData().games)
+			return _.chain(getLeagueData().games)
 					.filter(isTeamInGame)
 					.map(function(item){
 						var isTeam1 = (item.team1d === vm.teamId ? true: false);
 						var oponnentName = isTeam1 ? item.team2 : item.team1;
-						var scoreDisplay = getScoreDisplay(isTeam1, item.team1Score);
+						var scoreDisplay = getScoreDisplay(isTeam1, item.team1Score, item.team2Score);
 						return {
 							gameId: item.id,
 							opponent: oponnentName,
-							tiem: item.time,
+							time: item.time,
 							location: item.location,
 							locationUrl: item.locationUrl,
 							scoreDisplay: scoreDisplay,
